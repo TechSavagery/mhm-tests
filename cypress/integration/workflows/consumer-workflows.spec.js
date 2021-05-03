93409bf6271f412c99f7cabb058b6842834f411b
ladell.erby <ladellerby@techsavagery.net>
2021-05-03 01:14:28 -0700

    - Changed default timeout to 10 seconds - Added lighthouse performance test for home page - Adjusted selectors for mobile and ipad 'Consumer Find Therapist' tests - Added dependency injection for lighthouse and pa11y packages - Imported cypress-audint/commands for use within tests.

--- cypress/integration/workflows/consumer-workflows.spec.js
+++ cypress/integration/workflows/consumer-workflows.spec.js
@@ -71,11 +71,9 @@ describe("Consumer Workflow Tests", () => {
       //Click "Meet Your Matches" button 
       cy.get('#search-costs-match').click();
 
-      //Select Therapist From Result
-      cy.get('#\\31 590 > .p-0 > app-therapist-card > .d-md-block > div.row > .col-md-4 > .text-center > .mb-2').click();
+      //Click View PR
+      cy.get('#\\36 68 > .p-0 > app-therapist-card > .d-md-block > div.row > .col-md-4 > .text-center > .mb-2').click();
       cy.get('.px-5').click();
-
-      //Send Contact 
       cy.get('#nameC').clear();
       cy.get('#nameC').type('Test Consumer');
       cy.get(':nth-child(2) > .col-md-8 > .form-group > .form-control').clear();
@@ -84,16 +82,93 @@ describe("Consumer Workflow Tests", () => {
       cy.get('#seeakinC').type('(123) 456-7890');
       cy.get('.col-md-12 > .form-group > .form-control').click();
       cy.get('#contact-send-email').click();
-
-      //Click "Close"
       cy.get(':nth-child(3) > :nth-child(1) > .btn.ng-star-inserted').click();
-    });
 
+    });
     it("Consumer Find Therpist - Tablet #smoke", () => {
       cy.viewport('ipad-2')
+      cy.get('.special-zip-match > .input-group-append > .input-group-text > strong').click();
+      cy.get('.form-control').clear();
+      cy.get('.form-control').type('77019');
+      cy.get('.ng-untouched').click();
+      cy.get('#search-findmatch').click();
+
+      //Step 1
+      cy.get('#search-bar-2').click();
+
+      //Step 2 
+      cy.contains('How I feel').click();  
+      cy.get('#search-goals-next').click();
+      cy.contains('Lack of empathy').click();
+      cy.get('#search-goals-details-next').click();
+
+      //Step 3 
+      cy.contains('Join a support group').click();      
+      cy.get('#search-approaches-next').click();
+
+      //Step 4
+      cy.contains('Gender of therapist').click();
+      cy.contains('Male').click();
+      cy.get('#search-traits-next').click();
+
+      //Step 5 
+      cy.contains('Yes').eq(0).click();
+      cy.contains('AETNA').click();
+      cy.contains('Add more insurers').click();
+      cy.contains('ALLEGIAN').click();
+      cy.contains('Meet your matches').click();
+
+      //Contact Therapist 
+      cy.contains('VIEW PROFILE').eq(0).click({force: true});
+      cy.contains('CONTACT').first().click();
+      cy.get('#nameC').clear().type('Test Consumer');
+      cy.get('input[name="email"]').clear().type('andrew+test-contact-form@mentalhealthmatch.com');
+      cy.get('#seeakinC').clear().type('1234567890');
+      cy.get('#contact-send-email').click();
+      cy.contains('CLOSE').click();
+
     });
     it("Consumer Find Therpist - Mobile #smoke", () => {
       cy.viewport('iphone-8')
+      cy.get('.special-zip-match > .input-group-append > .input-group-text > strong').click();
+      cy.get('.form-control').clear();
+      cy.get('.form-control').type('77019');
+      cy.get('.ng-untouched').click();
+      cy.get('#search-findmatch').click();
+
+      //Step 1
+      cy.get('#search-bar-2').click();
+
+      //Step 2 
+      cy.contains('How I feel').click();  
+      cy.get('#search-goals-next').click();
+      cy.contains('Lack of empathy').click();
+      cy.get('#search-goals-details-next').click();
+
+      //Step 3 
+      cy.contains('Join a support group').click();      
+      cy.get('#search-approaches-next').click();
+
+      //Step 4
+      cy.contains('Gender of therapist').click();
+      cy.contains('Male').click();
+      cy.get('#search-traits-next').click();
+
+      //Step 5 
+      cy.contains('Yes').eq(0).click();
+      cy.contains('AETNA').click();
+      cy.contains('Add more insurers').click();
+      cy.contains('ALLEGIAN').click();
+      cy.contains('Meet your matches').click();
+
+      //Contact Therapist 
+      cy.contains('VIEW PROFILE').eq(0).click({force: true});
+      cy.contains('CONTACT').first().click();
+      cy.get('#nameC').clear().type('Test Consumer');
+      cy.get('input[name="email"]').clear().type('andrew+test-contact-form@mentalhealthmatch.com');
+      cy.get('#seeakinC').clear().type('1234567890');
+      cy.get('#contact-send-email').click();
+      cy.contains('CLOSE').click();
     });
   });
   
