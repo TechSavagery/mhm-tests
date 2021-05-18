1f3886fd9990587eac4e1008221a36efff6f1d3a
ladell.erby <ladellerby@techsavagery.net>
2021-05-18 00:51:18 -0700

    - update test data for qa env - add Therapist-Therapist Search by Zip test

--- cypress/integration/workflows/therapist-workflows.spec.js
+++ cypress/integration/workflows/therapist-workflows.spec.js
@@ -1,4 +1,4 @@
-describe("Therapist Workflow Tests", () => {
+describe("Therapist Login Tests", () => {
   beforeEach(() => {
     cy.window().then((win) => {
       win.sessionStorage.clear()
@@ -41,5 +41,37 @@ cy.visit(Cypress.env('baseUrl'));
     cy.get('#inputPassword').clear();
     cy.get('#inputPassword').type(Cypress.env('therapist-subscribed').password);
     cy.get('.btn').click();
+  });
+});
+
+describe("Therapist Therapist Search Tests", () => {
+  beforeEach(() => {
+    cy.window().then((win) => {
+      win.sessionStorage.clear()
+    })
+cy.clearCookies();
+cy.visit(Cypress.env('baseUrl'));
+  });
+
+  it.only("Search By Zip Code Returns Results #smoke", () => {
+    //Arrange
+    cy.viewport('macbook-13')
+    //Login
+    cy.visit(Cypress.env('baseUrl') + 'login');
+    cy.get('#inputEmail').clear();
+    cy.get('#inputEmail').type(Cypress.env('therapist-subscribed').email);
+    cy.get('#inputPassword').clear();
+    cy.get('#inputPassword').type(Cypress.env('therapist-subscribed').password);
+    cy.contains('LOG IN').click();
+
+    //Act
+    cy.contains('Find a Therapist',{matchCase: false}).click({force: true});
+    cy.get('[placeholder="e.g. 77019"]').click().type('77019');
+    cy.contains ('Start Search').first().click();
+    
+    //Assert
+    cy.url().should('include', '/therapist/search');
+    cy.contains('Contact',{matchCase: false}).its('length').should('be.lte', 5);
+    cy.contains('Showing 1 - 5',{matchCase: false}).its('length').should('be.eq', 1);
   });
 });
