18e2f32ecf3afa5046b5f145d4b4eb856b18ed27
ladellerby <ladellerby@gmail.com>
2023-04-12 00:39:25 -0700

    fix: typos in screenshotuploads

--- cypress/env/qa.config.js
+++ cypress/env/qa.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  videoCompression: false,
   experimentalStudio: true,
   retries: 1,
   defaultCommandTimeout: 10000,
